aedae4a12c176c79635606b22ccc08fa9d6866c6
danielbeinat <danielbeinat@outlook.es>
2024-12-15 09:23:35 -0300

    add files

--- src/pages/Students/Portal.tsx
+++ src/pages/Students/Portal.tsx
@@ -1,10 +1,8 @@
 import { Login } from "../Count/Login";
-import { Register } from "../Count/Register";
 export const Portal: React.FC = () => {
   return (
     <>
       <Login />
-      <Register />
     </>
   );
 };
